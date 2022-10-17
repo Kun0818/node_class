f5e744f2d8403910aeed1ff06af1694dba9aa955
Kunda <love60066@gmail.com>
2022-10-17 09:13:46 +0800

    總筆數

--- index.js
+++ index.js
@@ -129,6 +129,34 @@ app.get('/try-db', async (req, res) => {
   res.json(rows)
 })
 
+app.get('/try-db-add', async (req, res) => {
+  const name = "林克";
+  const email = "link@gmail.com";
+  const mobile = "0918555666";
+  const birthday = "1998-10-27";
+  const address = "宜蘭縣";
+  const sql = "INSERT INTO `address_book`(`name`, `email`, `mobile`, `birthday`, `address`, `created_at`) VALUES (?,?,?,?,?,now())";
+
+  const [{ result }] = await db.query(sql, [name, email, mobile, birthday, address]);
+  res.json(result);
+
+})
+
+app.get('/try-db-add2', async (req, res) => {
+  const name = "林克";
+  const email = "link@gmail.com";
+  const mobile = "0918555666";
+  const birthday = "1998-10-27";
+  const address = "宜蘭縣";
+  const sql = "INSERT INTO `address_book` SET ?";
+
+  const [result] = await db.query(sql, [{ name, email, mobile, birthday, address, create_at: new Date() }]);
+  res.json(result);
+
+})
+
+app.use('/ab',require(__dirname + '/routes/address-book'))
+
 
 //_________________________________________________________
 
